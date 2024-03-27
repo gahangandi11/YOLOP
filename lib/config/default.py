a3f8844345da9270af15253d267271f1696b6f56
Neema Jakisa <98976545+neemajakisa@users.noreply.github.com>
2024-03-27 02:59:17 -0500

    Update default.py

--- lib/config/default.py
+++ lib/config/default.py
@@ -5,7 +5,7 @@ from yacs.config import CfgNode as CN
 _C = CN()
 
 _C.LOG_DIR = 'runs/'
-_C.GPUS = (0,)     
+_C.GPUS = (0,1)     
 _C.WORKERS = 10
 _C.PIN_MEMORY = True
 _C.PRINT_FREQ = 2150
@@ -51,14 +51,11 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
 
-# _C.DATASET.DATAROOT = '/home/jakisa/Documents/HybridNets/datasets/imgs'       # the path of images folder
-# _C.DATASET.LABELROOT = '/home/jakisa/Documents/HybridNets/datasets/det_annot'      # the path of det_annotations folder
-# _C.DATASET.MASKROOT = '/home/jakisa/Documents/HybridNets/datasets/da_seg_annotations'                # the path of da_seg_annotations folder
-# _C.DATASET.LANEROOT = '/home/jakisa/Documents/HybridNets/datasets/ll_seg_annotations'               # the path of ll_seg_annotations folder
-_C.DATASET.DATAROOT = '/home/jakisa/Documents/YOLOP_tweak/mini_datasets/imgs'       # the path of images folder
-_C.DATASET.LABELROOT = '/home/jakisa/Documents/YOLOP_tweak/mini_datasets/det_annot'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '/home/jakisa/Documents/YOLOP_tweak/mini_datasets/da_seg_annotations'                # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = '/home/jakisa/Documents/YOLOP_tweak/mini_datasets/ll_seg_annotations'
+
+_C.DATASET.DATAROOT = 'home/data/yolop_tweak/datasets/imgs'       # the path of images folder
+_C.DATASET.LABELROOT = 'home/data/yolop_tweak/datasets/det_annot'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = 'home/data/yolop_tweak/datasets/da_seg_annotations'                # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = 'home/data/yolop_tweak/datasets/ll_seg_annotations'
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
@@ -95,10 +92,10 @@ _C.TRAIN.GAMMA1 = 0.99
 _C.TRAIN.GAMMA2 = 0.0
 
 _C.TRAIN.BEGIN_EPOCH = 0
-_C.TRAIN.END_EPOCH =  100 #300 #240 ############
+_C.TRAIN.END_EPOCH =  240 ############
 
 _C.TRAIN.VAL_FREQ = 20
-_C.TRAIN.BATCH_SIZE_PER_GPU = 16 #28 #32  ##10 #24 ##############
+_C.TRAIN.BATCH_SIZE_PER_GPU = 32  ##10 #24 ##############
 _C.TRAIN.SHUFFLE = True
 
 _C.TRAIN.IOU_THRESHOLD = 0.2 ###0.001
@@ -123,7 +120,7 @@ _C.TRAIN.PLOT = True                #
 
 # testing
 _C.TEST = CN(new_allowed=True)
-_C.TEST.BATCH_SIZE_PER_GPU = 16 #28 #32
+_C.TEST.BATCH_SIZE_PER_GPU = 32
 _C.TEST.MODEL_FILE = ''
 _C.TEST.SAVE_JSON = False
 _C.TEST.SAVE_TXT = False
